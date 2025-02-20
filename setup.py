13abb9f896268d67d47a955d5d17e848b7be4033
avrcal <105925321+avrcal@users.noreply.github.com>
2025-02-20 11:27:58 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="instapi",
+    version="0.2.0",
+    author="avrcal',
+    description="Instagram API client with event-driven architecture",
+    packages=find_packages(),
+    install_requires=[
+        "instagrapi>=1.20.0",
+        "python-dotenv>=1.0.0",
+        "aiohttp>=3.8.1"
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Application Frameworks"
+    ],
+    python_requires=">=3.8",
+    include_package_data=True
+)
