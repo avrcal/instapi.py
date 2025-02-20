e59d0e204ae0016cf401cffbb50341b69262647f
avrcal <105925321+avrcal@users.noreply.github.com>
2025-02-20 11:24:47 +0300

    Add files via upload

--- cogs/messages.py
+++ cogs/messages.py
@@ -1,1 +1,14 @@
+from ..models import Message
 
+class MessagesCog:
+    def __init__(self, client):
+        self.client = client
+
+    async def on_message(self, message: Message):
+        if message.text.startswith("/"):
+            await self.handle_command(message)
+
+    async def handle_command(self, message: Message):
+        command, *args = message.text[1:].split()
+        if command == "ping":
+            await self.client.send_message(message.sender.pk, "Pong!")
